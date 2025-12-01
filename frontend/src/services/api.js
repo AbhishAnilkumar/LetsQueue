78f0a5a6129c889776d85914cfcb3874e1961051
AbhishAnilkumar <abhish0210@gmail.com>
2025-12-01 19:59:00 +0530

    deploy frontend to vercel & connect backend to render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", 
+  baseURL: "https://letsqueue-backend-s0g8.onrender.com/", 
 });
 
 let anonId = localStorage.getItem("anon_token");
